89999b0499c434cf1f833277c3035cd4772d6b24
hassanalawie <hassan.alawie10@gmail.com>
2023-04-15 14:34:20 -0400

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
-import Button1 from './components/Button1';
-import Button2 from './components/Button2';
-import ButtonSubText from './components/ButtonSubText'
-import { OptionBar } from './components/OptionBar';
-import { TextInput } from './components/TextInput';
-import { COLORS } from './colors';
-import ButtonWithImage from './components/ButtonWithImage';
-import { Pagination } from './components/Pagination';
-import { Slider } from './components/Slider';
+import CreateEventPageName from "./pages/CreateEventNamePage";
+import HomePage from "./pages/HomePage";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Slider values={[40.2,22.8,10,4]}/>
-    </div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/createEvent/name" element={<CreateEventPageName/>} />
+      </Routes>
+    </Router>
+    
   );
 }
 
---
+++ src/pages/CreateEventNamePage.tsx
@@ -0,0 +1,10 @@
+
+function CreateEventPageName() {
+  return (
+<div>
+    <p>Create Event Name</p>
+</div>
+  );
+}
+
+export default CreateEventPageName;
